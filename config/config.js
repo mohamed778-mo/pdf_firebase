d50b749d84832adf50fde19bde10486f8f37fbed
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-08-10 23:02:18 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 
 
-const ATLAS_URL= process.env.ATLAS_URL
+const MONGOLINK= process.env.MONGOLINK
 const DBconnection =()=>{ 
-mongoose.connect('mongodb://127.0.0.1:27017/mydatabase')
+mongoose.connect(MONGOLINK)
 .then(()=>{console.log('done connection !!')})
 .catch((e)=>{console.log(e.message)})
 }
